12f7072d7e7ce3b712fad6c82e2fa3aaed1e07ae
Phalangs <p1miklautz@edu.aau.at>
2022-03-06 02:31:48 +0100

    attempting to connect via the method described in UE works in java, but not on button click?

--- app/src/main/java/com/example/se2testproject/MainActivity.java
+++ app/src/main/java/com/example/se2testproject/MainActivity.java
@@ -3,6 +3,14 @@ package com.example.se2testproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,78 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Thread thread = new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        try  {
+                            String sentence;
+                            String modifiedSentence;
+
+                            EditText edit1 = findViewById(R.id.serverInput);
+                            TextView text1 = findViewById(R.id.serverOutput);
+
+                            BufferedReader inFromUser = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(edit1.getText().toString().getBytes(StandardCharsets.UTF_8))));
+
+                            Socket clientSocket = new Socket ("se2-isys.aau.at",53212);
+
+                            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+
+                            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                            sentence=inFromUser.readLine();
+
+                            outToServer.writeBytes(sentence+'\n');
+
+                            modifiedSentence = inFromServer.readLine();
+
+                            //System.out.print("From server: "+modifiedSentence);
+                            text1.setText(modifiedSentence);
+
+                            clientSocket.close();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+                thread.start();
+
+            }
+        });
+    }
+
+
+    public void connectToServer(View view) throws IOException {
+        String sentence;
+        String modifiedSentence;
+
+        EditText edit1 = findViewById(R.id.serverInput);
+        TextView text1 = findViewById(R.id.serverOutput);
+
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(edit1.getText().toString().getBytes(StandardCharsets.UTF_8))));
+
+        Socket clientSocket = new Socket ("se2-isys.aau.at",53212);
+
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        sentence=inFromUser.readLine();
+
+        outToServer.writeBytes(sentence+'\n');
+
+        modifiedSentence = inFromServer.readLine();
+
+        //System.out.print("From server: "+modifiedSentence);
+        text1.setText(modifiedSentence);
+
+        clientSocket.close();
     }
 }
